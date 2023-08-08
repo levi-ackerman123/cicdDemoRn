6c34996eef315d2709be999d932e76a6ae2df81a
Poojan Bhatt <pdbhatt010@gmail.com>
2023-08-08 13:37:23 +0530

    updated-2 post clone build script

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -9,3 +9,5 @@ cd android/app/src/main/res/values
 echo "Change app name"
 xmlstarlet ed --inplace -u "/resources/string[@name='app_name']" --value "staging" strings.xml
 
+echo "Print content"
+cat $APPCENTER_SOURCE_DIRECTORY/android/app/src/main/res/values/strings.xml
