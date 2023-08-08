8290aa46a85a25b147b8e0a0079d761f308627e8
Poojan Bhatt <pdbhatt010@gmail.com>
2023-08-08 13:23:57 +0530

    post clone script added

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,1 +1,11 @@
 echo "MY CUSTOM POST-CLONE SCRIPT..."
+
+echo "Install xmlstarlet" 
+apt install xmlstarlet
+
+echo "Move to strings directory"
+cd android/app/src/main/res/values
+
+echo "Change app name"
+xmlstarlet ed --inplace -u "/resources/string[@name='app_name']" --value "staging" strings.xml
+
